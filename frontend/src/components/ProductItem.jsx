cf42c18ed9335f7da085e734a894e067e49cbaef
punsano26 <xlichaphlreuxn@gmail.com>
2025-01-09 11:47:36 +0700

    add Banner&ProductItem

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const ProductItem = ({ image, name, price, rate }) => {
+  return (
+    <div className="bg-white px-3 py-2 rounded-2xl flex items-center gap-3 shadow-sm w-64">
+      <img src={image} className="rounded-2xl w-20" alt="" />
+      <div className="space-y-1">
+        <h5>{name}</h5>
+        <div className="rating rating-sm">
+          <input
+            type="radio"
+            name="rating-4"
+            className="mask mask-star-2 bg-green-500"
+            disabled
+            defaultChecked={rate == 1 ? true : false}
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            className="mask mask-star-2 bg-green-500"
+            disabled
+            defaultChecked={rate == 2 ? true : false}
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            className="mask mask-star-2 bg-green-500"
+            disabled
+            defaultChecked={rate == 3 ? true : false}
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            className="mask mask-star-2 bg-green-500"
+            disabled
+            defaultChecked={rate == 4 ? true : false}
+          />
+          <input
+            type="radio"
+            name="rating-4"
+            className="mask mask-star-2 bg-green-500"
+            disabled
+            defaultChecked={rate == 5 ? true : false}
+          />
+        </div>
+        <p className="text-red">{price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- frontend/src/pages/Home/Banner.jsx
+++ frontend/src/pages/Home/Banner.jsx
@@ -1,7 +1,45 @@
 import React from "react";
-
+import ProductItem from "../../components/ProductItem";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
+      <div className="py-24 flex flex-col md:flex-row-reverse justify-between items-center">
+        <div className="md:w-1/2">
+          <img src="/images/home/banner.png" alt="banner" />
+          <div className="flex flex-col md:flex-row items-center justify-around mt-16 gap-4">
+            <ProductItem
+              image="/imgages/home/gamepad.png"
+              name="Game Pad"
+              rating="3"
+              price="400"
+            />
+            <ProductItem
+              image="/imgages/home/headphone.png"
+              name="headphone"
+              rating="4"
+              price="1000"
+            />
+          </div>
+        </div>
+        <div className="md:w-1/2 space-y-7 px-4">
+          <h2 className="md:text-4xl text-4xl font-bold md:leading-snug leading-snug">
+            Discover Uniq <span className="text-red">Software Engineering</span>{" "}
+            Swang for Every Coding En
+          </h2>
+          <p className="text-xl text-[#4A4A4A]">
+            our Mission: To merge the fashion with functionality in the world of
+            Sofware Engineering
+          </p>
+          <a
+            className="btn bg-red px-8 py-3 font-semibold text-while rounded-full"
+            href="/shop"
+          >
+            Order Now
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
