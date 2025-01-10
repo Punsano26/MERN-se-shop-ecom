e4c677e5551a459a405c7604cd602b33c555e8ee
punsano26 <xlichaphlreuxn@gmail.com>
2025-01-10 17:14:10 +0700

    slider card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+const sampleNextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return ( <div className={className} style={{...style,display:"block", background:"red"}}>Next</div>)
 const Card = () => {
   const { _id, name, image, description, cetagory, price } = item;
   const [isHeartfilled, setIsHeartfilled] = useState(false);
@@ -9,7 +11,7 @@ const Card = () => {
 
   return (
     <div className="card shadow-xl relative mr-5 md:my-5 h-120">
-      <div className="rating gap-1 absolute right-2 top-2 p-4 heartStar bg-red">
+      <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar`>}
         onClick={handleHeartClick}
       </div>
       <figure>
