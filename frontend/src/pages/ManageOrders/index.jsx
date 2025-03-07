cc110a120b7d449350aa296c18fe0b95e63caf34
Punsano26 <xlichaphlreuxn@gmail.com>
2025-03-07 12:34:13 +0700

    Add order management service and initial ManageOrders page

---
+++ frontend/src/pages/ManageOrders/index.jsx
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from 'react'
+import OrderService from '../../services/order.service';
+
+const index = () => {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        
+    }, []);
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
